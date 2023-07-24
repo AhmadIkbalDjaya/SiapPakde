657fa2ea2a709ddd04fc75029ea51a618f34fa1c
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-07-24 13:28:58 +0700

    kelembagaan

---
+++ resources/views/pages/user/kelembagaan_desa.blade.php
@@ -0,0 +1,404 @@
+@extends('layouts.user')
+
+@push('userStyle')
+  <link rel="stylesheet" href="{{ asset('css/user/kelembagaan_desa.css') }}">
+@endpush
+
+@section('content')
+<section id="hero" class="mt-5">
+  <div class="container text-center">
+    <div class="row pt-5 align-content-center">
+      <div class="col-lg-12 col-md-12">
+        <p class="title">
+          Kelembagaan Desa TondongKura
+        </p>
+        <p class="sub-title"> Kec. Pangkajene, Kabupaten Pangkajene Dan Kepulauan, Sulawesi Selatan</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section id="kelembagaan" class="py-5">
+  <div class="container">
+    <div class="row mb-3">
+      <div class="col-md-12">
+        <h2 class="heading">Kelembagaan</h2>
+      </div>
+    </div>
+    {{-- bpd --}}
+    <div class="row bpd mb-5">
+      <div class="col-12">
+        <p class="nama-lembaga">
+          Badan Permusyawaratan Desa
+        </p>
+      </div>
+      <div class="col-12">
+        <div class="user-container">
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- kader pkk --}}
+    <div class="row bpd mb-5">
+      <div class="col-12">
+        <p class="nama-lembaga">
+          Kader PKK
+        </p>
+      </div>
+      <div class="col-12">
+        <div class="user-container">
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- posyandu --}}
+    <div class="row bpd mb-5">
+      <div class="col-12">
+        <p class="nama-lembaga">
+          Kader Posyandu
+        </p>
+      </div>
+      <div class="col-12">
+        <div class="user-container">
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- kpm --}}
+    <div class="row bpd mb-5">
+      <div class="col-12">
+        <p class="nama-lembaga">
+          Kader Pembangunan Manusia
+        </p>
+      </div>
+      <div class="col-12">
+        <div class="user-container">
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- karang taruna --}}
+    <div class="row bpd mb-5">
+      <div class="col-12">
+        <p class="nama-lembaga">
+          Karang Taruna
+        </p>
+      </div>
+      <div class="col-12">
+        <div class="user-container">
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- lpm --}}
+    <div class="row bpd mb-5">
+      <div class="col-12">
+        <p class="nama-lembaga">
+          Lembaga Pemberdayaan Manusia
+        </p>
+      </div>
+      <div class="col-12">
+        <div class="user-container">
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+          <div class="user-box mx-3">
+            <div class="photo-box">
+              <img src="..." class="img-fluid user-photo" alt="...">
+            </div>
+            <p class="name">Ahmad Ikbal Djaya</p>
+            <p class="position">Kepala Desa</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+@endsection
