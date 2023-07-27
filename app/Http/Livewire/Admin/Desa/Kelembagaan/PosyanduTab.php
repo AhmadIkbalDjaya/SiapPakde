1dac83a7ed6318381995b72a67a44556ea15c92e
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-07-27 16:36:34 +0700

    posyandu and kader crud

---
+++ app/Http/Livewire/Admin/Desa/Kelembagaan/PosyanduTab.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Desa\Kelembagaan;
+
+use App\Models\KaderPosyandu;
+use App\Models\Posyandu;
+use Livewire\Component;
+
+class PosyanduTab extends Component
+{
+    public $desa;
+
+    public $posyandu_id, $nama_posyandu;
+    public $posyandu_edit_id;
+
+    public $kader_posyandu_id, $nama, $jabatan, $select_posyandu_id;
+    public $kader_posyandu_edit_id;
+
+    public function render()
+    {
+        $posyandus = Posyandu::where('desa_id', $this->desa->id)->get();
+        $kader_posyandus = KaderPosyandu::whereIn('posyandu_id', $posyandus->pluck('id'))->get();
+        return view('livewire.admin.desa.kelembagaan.posyandu-tab', [
+            "posyandus" => $posyandus,
+            "kader_posyandus" => $kader_posyandus,
+        ]);
+    }
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            "nama_posyandu" => "required|string",
+            "nama" => "required|string",
+            "jabatan" => "required|string",
+            "select_posyandu_id" => "required|exists:posyandus,id",
+        ]);
+    }
+
+    public function storePosyandu()
+    {
+        $validated = $this->validate([
+            "nama_posyandu" => "required|string",
+        ]);
+        $validated['nama'] = $validated["nama_posyandu"];
+        $validated['desa_id'] = $this->desa->id;
+        Posyandu::create($validated);
+        session()->flash('success', "Posyandu Berhasil Ditambahkan");
+        $this->resetPosyanduField();
+        $this->dispatchBrowserEvent("close-modal");
+    }
+
+    public function storeKaderPosyandu() {
+        $validated = $this->validate([
+            "nama" => "required|string",
+            "jabatan" => "required|string",
+            "select_posyandu_id" => "required|exists:posyandus,id",
+        ]);
+        $validated['posyandu_id'] = $validated['select_posyandu_id'];
+        KaderPosyandu::create($validated);
+        session()->flash('success', "Kader Posyandu Berhasil Ditambahkan");
+        $this->resetKaderPosyanduField();
+        $this->dispatchBrowserEvent("close-modal");
+    }
+
+    public function updatePosyandu(Posyandu $posyandu)
+    {
+        $validated = $this->validate([
+            "nama_posyandu" => "required|string",
+        ]);
+        $validated['nama'] = $validated["nama_posyandu"];
+        $posyandu->update($validated);
+        session()->flash('success', "Posyandu Berhasil Diupdate");
+        $this->resetPosyanduField();
+        $this->dispatchBrowserEvent("close-modal");
+    }
+
+    public function updateKaderPosyandu(KaderPosyandu $kaderPosyandu) {
+        $validated = $this->validate([
+            "nama" => "required|string",
+            "jabatan" => "required|string",
+            "select_posyandu_id" => "required|exists:posyandus,id",
+        ]);
+        $validated['posyandu_id'] = $validated['select_posyandu_id'];
+        $kaderPosyandu->update($validated);
+        session()->flash('success', "Kader Posyandu Berhasil Diupdate");
+        $this->resetKaderPosyanduField();
+        $this->dispatchBrowserEvent("close-modal");
+    }
+
+    public function destroyPosyandu(Posyandu $posyandu)
+    {
+        $posyandu->delete();
+        session()->flash('danger', "Posyandu Berhasil Dihapus");
+        $this->resetPosyanduField();
+        $this->dispatchBrowserEvent("close-modal");
+    }
+
+    public function destroyKaderPosyandu(KaderPosyandu $kaderPosyandu)
+    {
+        $kaderPosyandu->delete();
+        session()->flash('danger', "Kader Posyandu Berhasil Dihapus");
+        $this->resetKaderPosyanduField();
+        $this->dispatchBrowserEvent("close-modal");
+    }
+
+    public function resetPosyanduField()
+    {
+        $this->nama_posyandu = null;
+        $this->posyandu_id = null;
+        $this->posyandu_edit_id = null;
+    }
+
+    public function resetKaderPosyanduField(){
+        $this->kader_posyandu_id = null;
+        $this->nama = null;
+        $this->jabatan = null;
+        $this->select_posyandu_id = null;
+        $this->kader_posyandu_edit_id = null;
+    }
+
+    public function setPosyanduField(Posyandu $posyandu) {
+        $this->nama_posyandu = $posyandu->nama;
+        $this->posyandu_id = $posyandu->id;
+        $this->posyandu_edit_id = $posyandu->id;
+    }
+
+    public function setKaderPosyanduField(KaderPosyandu $kaderPosyandu) {
+        $this->kader_posyandu_id = $kaderPosyandu->id;
+        $this->nama = $kaderPosyandu->nama;
+        $this->jabatan = $kaderPosyandu->jabatan;
+        $this->select_posyandu_id = $kaderPosyandu->posyandu_id;
+        $this->kader_posyandu_edit_id = $kaderPosyandu->id;
+    }
+}
---
+++ resources/views/livewire/admin/desa/kelembagaan/posyandu-tab.blade.php
@@ -0,0 +1,356 @@
+<div wire:ignore.self class="tab-pane fade pt-3" id="posyandu-desa">
+  <div class="row">
+    {{-- posyandu --}}
+    <div class="col-md-4">
+      <h5 class="card-title"> posyandu</h5>
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modal-create-posyandu"
+        wire:click='resetPosyanduField'>
+        <i class="bi bi-plus-lg"></i> Posyandu
+      </button>
+      <!-- Modal Create  posyandu -->
+      <div wire:ignore.self class="modal fade" id="modal-create-posyandu" tabindex="-1"
+        aria-labelledby="modal-create-posyanduLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="modal-create-posyanduLabel">Tambah Posyandu</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form wire:submit.prevent='storePosyandu' action="">
+                <div class="row mb-3">
+                  <label for="nama_posyandu" class="col-md-4 col-lg-3 col-form-label">
+                    Nama Posyandu
+                  </label>
+                  <div class="col-md-8 col-lg-9">
+                    <input wire:model='nama_posyandu' name="nama_posyandu" type="text"
+                      class="form-control @error('nama_posyandu') is-invalid @enderror" id="nama_posyandu"
+                      value="">
+                    @error('nama_posyandu')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Tambah</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      {{-- End Modal Create  posyandu --}}
+
+      <!-- Table with hoverable rows -->
+      @if ($posyandus->count() > 0)
+        <div class="table-responsive">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col" style="white-space: nowrap">No</th>
+                <th scope="col" style="white-space: nowrap">Nama</th>
+                <th scope="col" style="white-space: nowrap">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($posyandus as $posyandu)
+                <tr>
+                  <th scope="row" style="white-space: nowrap">{{ $loop->iteration }}</th>
+                  <td style="white-space: nowrap">{{ $posyandu->nama }}</td>
+                  <td style="white-space: nowrap">
+                    <span wire:click='setPosyanduField({{ $posyandu->id }})' class="badge bg-warning text-white"
+                      data-bs-toggle="modal" data-bs-target="#modal-edit-posyandu" style="cursor: pointer">
+                      <i class="bi bi-pencil-square"></i>
+                    </span>
+                    <span wire:click='setPosyanduField({{ $posyandu->id }})' class="badge bg-danger"
+                      data-bs-toggle="modal" data-bs-target="#modal-delete-posyandu" style="cursor: pointer">
+                      <i class="bi bi-trash"></i>
+                    </span>
+                  </td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      @else
+        <h5 class="text-center">
+          Posyandu Belum Ditambahkan
+        </h5>
+      @endif
+      <!-- End Table with hoverable rows -->
+
+      <!-- Modal Edit  posyandu -->
+      <div wire:ignore.self class="modal fade" id="modal-edit-posyandu" tabindex="-1"
+        aria-labelledby="modal-edit-posyanduLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="modal-edit-posyanduLabel">Edit Posyandu</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form wire:submit.prevent='updatePosyandu({{ $posyandu_edit_id }})' action="">
+                <div class="row mb-3">
+                  <label for="nama_posyandu" class="col-md-4 col-lg-3 col-form-label">
+                    Nama Posyandu
+                  </label>
+                  <div class="col-md-8 col-lg-9">
+                    <input wire:model='nama_posyandu' name="nama_posyandu" type="text"
+                      class="form-control @error('nama_posyandu') is-invalid @enderror" id="nama_posyandu"
+                      value="">
+                    @error('nama_posyandu')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-warning">Edit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      {{-- End Modal Edit  posyandu --}}
+
+      {{-- Modal Delete  posyandu --}}
+      <div wire:ignore.self class="modal fade" id="modal-delete-posyandu" tabindex="-1"
+        aria-labelledby="modal-delete-posyanduLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="modal-delete-bumdesLabel">Konfirmasi Hapus Posyandu</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+              <h5>Yakin Ingin Menghapus {{ $nama_posyandu }}?</h5>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button wire:click='destroyPosyandu({{ $posyandu_id }})' type="button"
+                class="btn btn-danger">Hapus</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {{-- End Modal Delete  posyandu --}}
+    </div>
+
+    {{-- kader posyandu --}}
+    <div class="col-md-8">
+      <h5 class="card-title">Kader Posyandu</h5>
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
+        data-bs-target="#modal-create-kader-posyandu">
+        <i class="bi bi-plus-lg"></i> Kader Posyandu
+      </button>
+      <!-- Modal Create kader posyandu -->
+      <div wire:ignore.self class="modal fade" id="modal-create-kader-posyandu" tabindex="-1"
+        aria-labelledby="modal-create-kader-posyanduLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="modal-create-kader-posyanduLabel">Tambah posyandu</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form wire:submit.prevent='storeKaderPosyandu' action="">
+                <div class="row mb-3">
+                  <label for="nama" class="col-md-4 col-lg-3 col-form-label">
+                    Nama
+                  </label>
+                  <div class="col-md-8 col-lg-9">
+                    <input wire:model='nama' name="nama" type="text"
+                      class="form-control @error('nama') is-invalid @enderror" id="nama" value="">
+                    @error('nama')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="jabatan" class="col-md-4 col-lg-3 col-form-label">
+                    Jabatan
+                  </label>
+                  <div class="col-md-8 col-lg-9">
+                    <input wire:model='jabatan' name="jabatan" type="text"
+                      class="form-control @error('jabatan') is-invalid @enderror" id="jabatan" value="">
+                    @error('jabatan')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="select_posyandu_id" class="col-md-4 col-lg-3 col-form-label">Posyandu</label>
+                  <div class="col-md-8 col-lg-9">
+                    <select wire:model='select_posyandu_id'
+                      class="form-select @error('select_posyandu_id') is-invalid @enderror"
+                      aria-label="Default select example" id="select_posyandu_id">
+                      <option hidden>Pilih Posyandu</option>
+                      @foreach ($posyandus as $posyandu)
+                        <option value="{{ $posyandu->id }}">{{ $posyandu->nama }}</option>
+                      @endforeach
+                    </select>
+                    @error('select_posyandu_id')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Tambah</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      {{-- End Modal Create kader posyandu --}}
+
+      <!-- Table with hoverable rows -->
+      @if ($kader_posyandus->count() > 0)
+        <div class="table-responsive">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col" style="white-space: nowrap">No</th>
+                <th scope="col" style="white-space: nowrap">Nama</th>
+                <th scope="col" style="white-space: nowrap">Jabatan</th>
+                <th scope="col" style="white-space: nowrap">Posyandu</th>
+                <th scope="col" style="white-space: nowrap">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($kader_posyandus as $kader_posyandu)
+                <tr>
+                  <th scope="row" style="white-space: nowrap">{{ $loop->iteration }}</th>
+                  <td style="white-space: nowrap">{{ $kader_posyandu->nama }}</td>
+                  <td style="white-space: nowrap">{{ $kader_posyandu->jabatan }}</td>
+                  <td style="white-space: nowrap">{{ $kader_posyandu->posyandu->nama }}</td>
+                  <td style="white-space: nowrap">
+                    <span wire:click='setKaderPosyanduField({{ $kader_posyandu->id }})' class="badge bg-warning text-white" data-bs-toggle="modal"
+                      data-bs-target="#modal-edit-kader-posyandu" style="cursor: pointer">
+                      <i class="bi bi-pencil-square"></i>
+                    </span>
+                    <span wire:click='setKaderPosyanduField({{ $kader_posyandu->id }})' class="badge bg-danger" data-bs-toggle="modal"
+                      data-bs-target="#modal-delete-kader-posyandu" style="cursor: pointer">
+                      <i class="bi bi-trash"></i>
+                    </span>
+                  </td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      @else
+        <h5 class="text-center">
+          Kader Posyandu Belum Ditambahkan
+        </h5>
+      @endif
+      <!-- End Table with hoverable rows -->
+
+      <!-- Modal Edit kader posyandu -->
+      <div wire:ignore.self class="modal fade" id="modal-edit-kader-posyandu" tabindex="-1"
+        aria-labelledby="modal-edit-kader-posyanduLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="modal-edit-kader-posyanduLabel">Edit posyandu</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form wire:submit.prevent='updateKaderPosyandu({{ $kader_posyandu_edit_id }})' action="">
+                <div class="row mb-3">
+                  <label for="nama" class="col-md-4 col-lg-3 col-form-label">
+                    Nama
+                  </label>
+                  <div class="col-md-8 col-lg-9">
+                    <input wire:model='nama' name="nama" type="text"
+                      class="form-control @error('nama') is-invalid @enderror" id="nama" value="">
+                    @error('nama')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="jabatan" class="col-md-4 col-lg-3 col-form-label">
+                    Jabatan
+                  </label>
+                  <div class="col-md-8 col-lg-9">
+                    <input wire:model='jabatan' name="jabatan" type="text"
+                      class="form-control @error('jabatan') is-invalid @enderror" id="jabatan" value="">
+                    @error('jabatan')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="select_posyandu_id" class="col-md-4 col-lg-3 col-form-label">Posyandu</label>
+                  <div class="col-md-8 col-lg-9">
+                    <select wire:model='select_posyandu_id'
+                      class="form-select @error('select_posyandu_id') is-invalid @enderror"
+                      aria-label="Default select example" id="select_posyandu_id">
+                      <option hidden>Pilih Posyandu</option>
+                      @foreach ($posyandus as $posyandu)
+                        <option value="{{ $posyandu->id }}">{{ $posyandu->nama }}</option>
+                      @endforeach
+                    </select>
+                    @error('select_posyandu_id')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-warning">Edit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      {{-- End Modal Edit kader posyandu --}}
+
+      {{-- Modal Delete kader posyandu --}}
+      <div wire:ignore.self class="modal fade" id="modal-delete-kader-posyandu" tabindex="-1"
+        aria-labelledby="modal-delete-kader-posyanduLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="modal-delete-bumdesLabel">Konfirmasi Hapus posyandu</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+              <h5>Yakin Ingin Menghapus {{ $nama }}?</h5>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button wire:click='destroyKaderPosyandu({{ $kader_posyandu_id }})' type="button" class="btn btn-danger">Hapus</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {{-- End Modal Delete kader posyandu --}}
+
+    </div>
+  </div>
+  @include('components.toast')
+</div>
